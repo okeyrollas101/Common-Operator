de9d7b5c524a8a9491a690e86575f861a4026c58
OKECHUKWU <obilekekachi@gmail.com>
2021-04-20 10:30:32 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Common_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // // + -, *, /, %
+            // int numerator = 10;
+            // int denominator = 3;
+            //   int result = numerator % denominator;
+            //   Console.WriteLine(result);
+           
+             // Ternary operator
+                // int x = 20;
+                // int y = 10;
+                // string result = x > y ? "x is greater than y" : "x is less than y";
+                // Console.WriteLine(result);
+
+              int x = 10;
+              x += 4;
+              Console.WriteLine(x);
+        }
+    }     
+}
